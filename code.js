8db2cd5074f150c3fe25bb1aa134d7996f866b6e
bdkopen <bdkopen@gmail.com>
2016-12-21 13:02:19 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,23 +1,11 @@
 var sketchProc=function(processingInstance){ with (processingInstance){
+setup = function() {
+    size(600,350);
+    noLoop();
+    background(0, 0, 0);
+  };
 
-size(650, 400); 
-frameRate(30);
-
-
-background(100,100,100);
-fill(255,255,255);
-
-// draw a rectangle
-rect(50, 20, 200, 300);
-
-// draw an ellipse
-ellipse(270, 210, 200, 160);
-
-// draw a line
-line(100, 100, 200, 200);
-fill(0,0,0);
-textSize(30);
-text("Hello World!",73,73);
-
-
-		}};
+  draw = function() {  
+    println("Hi");
+  };
+}};
