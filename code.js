4df3975b7a4d162c152cde6cfca4c84e4e38dda6
bdkopen <bdkopen@gmail.com>
2016-12-22 08:00:56 -0500

    Update code.js

--- code.js
+++ code.js
@@ -3,12 +3,25 @@ var sketchProc=function(processingInstance){ with (processingInstance){
         var windowWidth = window.innerWidth;
         var windowHeight = window.innerHeight;
         size(windowWidth/2, windowHeight/2);
+        frameRate(20);
     };
-
+    
+    //[{state: , speed: , temperature:}]
+    var particles = [];
+    
+    var createParticle(state, speed, temperature, locationX, locationY) {
+        particles.push({state:speed, speed:speed, temperature:temperature});
+    }
+    
     draw = function() {  
         background(255, 255, 255);
         fill(0, 0, 255);
         text("I know where you live at night", 200, 200);
         println("Hi");
     };
+    
+    mouseClicked = function() {
+        particles.push(1, 2, 3);
+        println(particles);
+    }
 }};
