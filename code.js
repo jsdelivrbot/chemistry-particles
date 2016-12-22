6d653c0f925b5804386a6ac9273f6bc1cc8371d4
bdkopen <bdkopen@gmail.com>
2016-12-21 20:32:10 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,11 +1,10 @@
 var sketchProc=function(processingInstance){ with (processingInstance){
     setup = function() {
         size(screen.width/2,screen.height/2);
-        noLoop();
-        background(255, 255, 255);
     };
 
     draw = function() {  
+        background(255, 255, 255);
         fill(0, 0, 255);
         text("I know where you live at night", 200, 200);
         println("Hi");
