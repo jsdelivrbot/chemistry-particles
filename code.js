eff608dbd5c93e941ee4e4102d2d674acfd4ed63
bdkopen <bdkopen@gmail.com>
2016-12-22 08:44:15 -0500

    Added drawParticle function.

--- code.js
+++ code.js
@@ -4,7 +4,7 @@ var sketchProc=function(processingInstance){ with (processingInstance){
         var windowHeight = window.innerHeight;
         size(windowWidth/2, windowHeight/2);
         frameRate(20);
-        background(0, 10, 0);
+        //background(0, 10, 0);
     };
     
     var heat_source_temperature = 0; //Celcius, -25 upto 100.
@@ -12,21 +12,30 @@ var sketchProc=function(processingInstance){ with (processingInstance){
     //[{state: , speed: , temperature:}]
     var particles = [];
     
-    var createParticle = function(state, speed, temperature, locationX, locationY) {
-        particles.push([state, speed, temperature, locationX, locationY]);
+    var createParticle = function(state, speed, temperature, locationX, locationY, directionX, directionY) {
+        particles.push([state, speed, temperature, locationX, locationY, directionX, directionY]);
         println("Hi");
     };
+    
+    var drawParticle = function(state, speed, temperature, locationX, locationY) {
+        fill(0, 0, 0);
+        ellipse(locationX, locationY, 10, 10);
+    };
+    
     
     draw = function() {  
         background(255, 255, 255);
         fill(0, 0, 255);
         text("I know where you live at night", 200, 200);
         //println("Hi");
+        for(var a = 0; a < particles.length; a++) {
+            drawParticle(particles[a][0], particles[a][1], particles[a][2], particles[a][3], particles[a][4], particles[a][5], particles[a][6]);
+        }
     };
     
     mouseClicked = function() {
         println("test");
-        createParticle(1,2,3,mouseX,mouseY);
+        createParticle(1, 2, 3, mouseX, mouseY, random(1, -1), random(1, -1));
         println(particles);
     };
 }};
