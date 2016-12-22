9dd967b70455e03a28975e84fc30064bffed14d7
bdkopen <bdkopen@gmail.com>
2016-12-22 16:17:23 -0500

    Create screen_right.js

---
+++ javascript/screen_right.js
@@ -0,0 +1,8 @@
+
+var sketchProc_right=function(processingInstance){ with (processingInstance){
+  setup = function() {
+    size(innerWidth/4, innerHeight/2);
+    background(0, 0, 0);
+  };
+
+}};
