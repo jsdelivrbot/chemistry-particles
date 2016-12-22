c8b666df2f861d04f9daa6d62c23fe44e44798be
bdkopen <bdkopen@gmail.com>
2016-12-22 16:21:04 -0500

    Update screen_left.js

--- javascript/screen_left.js
+++ javascript/screen_left.js
@@ -1,6 +1,6 @@
 var sketchProc_left=function(processingInstance){ with (processingInstance){
   setup = function() {
-    size(innerWidth/4, innerHeight/2);
+    size(innerWidth/5, innerHeight/2);
     background(0, 0, 0);
   };
 
