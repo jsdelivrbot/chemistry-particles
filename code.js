c845c14ff5438ae76acb7fddf38d433f79bd7541
bdkopen <bdkopen@gmail.com>
2016-12-21 20:22:48 -0500

    Update code.js
    Fixed indentation

--- code.js
+++ code.js
@@ -1,13 +1,13 @@
 var sketchProc=function(processingInstance){ with (processingInstance){
-setup = function() {
-    size(600,350);
-    noLoop();
-    background(0, 0, 0);
-  };
+    setup = function() {
+        size(600,350);
+        noLoop();
+        background(0, 0, 0);
+    };
 
-  draw = function() {  
-    fill(0, 0, 255);
-    text("I know where you live at night", 200, 200);
-    println("Hi");
-  };
+    draw = function() {  
+        fill(0, 0, 255);
+        text("I know where you live at night", 200, 200);
+        println("Hi");
+    };
 }};
