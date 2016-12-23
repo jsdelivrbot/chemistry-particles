41fae466f22c2345ca6deed24af45bd02ae2a092
bdkopen <bdkopen@gmail.com>
2016-12-23 12:05:43 -0500

    Update screen_center.js

--- javascript/screen_center.js
+++ javascript/screen_center.js
@@ -35,8 +35,8 @@ var sketchProc=function(processingInstance){ with (processingInstance){
             }
             
             for(var b = 0; b < particles.length; b++) {
-                if(particles[a] === particles[b]) {
-                    println("test2");
+                if(a === b) {
+                    //println("test2");
                     continue;
                 } else if(particles[a][3]+10 > particles[b][3] && particles[a][3]-10 < particles[b][3] && particles[a][4]+10 > particles[b][4] && particles[a][4]+10 < particles[b][4]) {
                     var temp = particles[a][5]
