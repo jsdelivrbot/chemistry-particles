0f6c9aa1fa2d0a1c03e38b442488c029120daa0d
bdkopen <bdkopen@gmail.com>
2016-12-29 18:23:10 -0500

    Update variables.js

--- javascript/variables.js
+++ javascript/variables.js
@@ -2,6 +2,6 @@ var windowWidth = window.innerWidth;
 var windowHeight = window.innerHeight;
 var heat_source_temperature = 0;
 var particles = [];
-if(windowWidth/4 <= 150 && windowHeight/2 <= 200) {
+if(windowWidth/4 <= 150 || windowHeight/2 <= 200) {
   alert("Your window is too small! Some functions may not perform correctly or may not work at all. Increase your window width to atleast 600 pixels wide and 200 pixels tall for optimal performance.");  
 }
