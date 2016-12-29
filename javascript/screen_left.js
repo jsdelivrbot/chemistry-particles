17b21cc5863d97010ce9585d68d2bfbe26b861a3
bdkopen <bdkopen@gmail.com>
2016-12-29 18:30:07 -0500

    Update screen_left.js

--- javascript/screen_left.js
+++ javascript/screen_left.js
@@ -2,11 +2,17 @@ var sketchProc_left=function(processingInstance){ with (processingInstance){
     setup = function() {
         size(innerWidth/4-5, innerHeight/2);
         background(227, 220, 209);
+        strokeWeight(3);
     };
-  
-  draw = function() {
-      background(227, 220, 209);
-      
-  };
+    var barX = heat_source_temperature;
+    var changeSourceTemperature = function() {
+        
+    }
+    
+    draw = function() {
+        background(227, 220, 209);
+        //line centered in canvas. -25 to 125
+        line(((innerWidth/4)/2)-75, 30, ((innerWidth/4)/2)+75, 30);
+    };
 
 }};
