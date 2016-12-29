3c57dd95d670b98dec7698e18a197d7b0909f676
bdkopen <bdkopen@gmail.com>
2016-12-29 17:01:01 -0500

    Update screen_right.js

--- javascript/screen_right.js
+++ javascript/screen_right.js
@@ -1,7 +1,7 @@
 
 var sketchProc_right=function(processingInstance){ with (processingInstance){
   setup = function() {
-    size(innerWidth/4, innerHeight/2);
+    size(innerWidth/4-5, innerHeight/2);
     background(0, 0, 0);
   };
 
