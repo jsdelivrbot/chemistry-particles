f65d354d4f7b512a1054df6dda77d8fe81fa966d
bdkopen <bdkopen@gmail.com>
2016-12-29 20:20:53 -0500

    Update screen_left.js

--- javascript/screen_left.js
+++ javascript/screen_left.js
@@ -4,15 +4,32 @@ var sketchProc_left=function(processingInstance){ with (processingInstance){
         background(227, 220, 209);
         strokeWeight(3);
     };
+    //define the x location of drag bar.
     var barX = heat_source_temperature;
     var changeSourceTemperature = function() {
-        
+        heat_source_temperature = barX;
     }
     
     draw = function() {
         background(227, 220, 209);
         //line centered in canvas. -25 to 125
         line((windowWidth/4)*0.25, 30, (windowWidth/4)*0.75, 30);
+        fill(0, 0, 0);
+        ellipse(barX, 30, 10, 10);
+    };
+    
+    mousePressed = function() {
+        if(mouseX >= (windowWidth/4)*0.25 && mouseX <= (windowWidth/4)*0.75) {
+            barX = mouseX;
+        }
+    };
+    
+    mouseClicked = function() {
+        
+    };
+    
+    mouseReleased = function() {
+        
     };
 
 }};
