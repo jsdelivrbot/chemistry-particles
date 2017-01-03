87dbc9846a93dd87c836661dd8ed7d54de5b44ac
bdkopen <bdkopen@gmail.com>
2017-01-02 19:12:10 -0500

    Update screen_left.js

--- javascript/screen_left.js
+++ javascript/screen_left.js
@@ -14,18 +14,15 @@ var sketchProc_left=function(processingInstance){ with (processingInstance){
     var screenLeftUpdate = function() {
         background(227, 220, 209);
         //line centered in canvas. -25 to 125
-        text("Heat Source Temperature (°C)", (windowWidth/4)*0.5, 15);
+        text("Heat Source Temperature (°C)", (windowWidth/4)*0.5, 10);
         line((windowWidth/4)*0.25, 30, (windowWidth/4)*0.75, 30);
         fill(0, 0, 0);
         ellipse(barX, 30, 10, 10);
     };
-    
-    draw = function() {
-        
-    };
+    screenLeftUpdate();
     
     mouseDragged = function() {
-        if(mouseX >= (windowWidth/4)*0.25 && mouseX <= (windowWidth/4)*0.75) {
+        if(mouseX >= (windowWidth/4)*0.25 && mouseX <= (windowWidth/4)*0.75 && mouseY > 20 && mouseY < 40) {
             barX = mouseX;
             changeSourceTemperature();
         }
