7e5a84e5e8c650fd4fa516684c32e6632e745406
bdkopen <bdkopen@gmail.com>
2017-01-12 17:29:56 -0500

    Update screen_left.js

--- javascript/screen_left.js
+++ javascript/screen_left.js
@@ -3,13 +3,18 @@ var sketchProc_left=function(processingInstance){ with (processingInstance){
         size(windowWidth/4-5, windowHeight/2);
         strokeWeight(3);
         textAlign(CENTER, TOP);
+        background(227, 220, 209);
+        //line centered in canvas. -25 to 125
+        fill(0, 0, 0);
+        text("Heat Source Temperature (°C)", (windowWidth/4)*0.5, 10);
+        line((windowWidth/4)*0.25, 30, (windowWidth/4)*0.75, 30);
+        ellipse(barX, 30, 10, 10);
     };
     //define the x location of drag bar.
     var barX = windowWidth/4*0.5;
     var changeSourceTemperature = function() {
         heat_source_temperature = barX;
     }
-    
     var screenLeftUpdate = function() {
         background(227, 220, 209);
         //line centered in canvas. -25 to 125
@@ -19,7 +24,7 @@ var sketchProc_left=function(processingInstance){ with (processingInstance){
         ellipse(barX, 30, 10, 10);
         //screenLeftUpdate();
     };
-    screenLeftUpdate();
+    //screenLeftUpdate();
     
     mouseDragged = function() {
         if(mouseX >= (windowWidth/4)*0.25 && mouseX <= (windowWidth/4)*0.75 && mouseY > 20 && mouseY < 40) {
